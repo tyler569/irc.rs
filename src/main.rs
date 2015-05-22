fc5d52f626f6d87647ab4e8bb782a3292226fb28
tyler569 <t5j6p9@gmail.com>
2015-05-22 07:50:54 -0400

    retab-ed

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,19 @@ use parser::Line;
 
 fn main() {
 
-	let name = "irc.freenode.net:6667";	
-	let con = TcpStream::connect(name).ok().unwrap();
-	let rbuf = BufReader::new(con.try_clone().ok().unwrap());
-	let mut wbuf = con.broadcast(stdout());
-	write!(&mut wbuf, "NICK tbot_3\r\n").ok().unwrap();
-	write!(&mut wbuf, "USER tbot_3 0 * :tbot - new and improved\r\n").ok().unwrap();
-	write!(&mut wbuf, "JOIN #openredstone\r\n").ok().unwrap();
-	for line in rbuf.lines() {
+    let name = "irc.freenode.net:6667"; 
+    let con = TcpStream::connect(name).ok().unwrap();
+    let rbuf = BufReader::new(con.try_clone().ok().unwrap());
+    let mut wbuf = con.broadcast(stdout());
+    write!(&mut wbuf, "NICK tbot_3\r\n").ok().unwrap();
+    write!(&mut wbuf, "USER tbot_3 0 * :tbot - new and improved\r\n").ok().unwrap();
+    write!(&mut wbuf, "JOIN #openredstone\r\n").ok().unwrap();
+    for line in rbuf.lines() {
         let linestr = line.ok().unwrap();
         let lp = Line::parse(linestr).ok().unwrap();
         println!("{:?} {:?} {:?}", lp.prefix(), lp.command(), lp.params());
         if lp.command() == "PING" {
             write!(&mut wbuf, "PONG :{}\n", lp.params().last().unwrap()).ok().unwrap();
         }
-	}
+    }
 }
