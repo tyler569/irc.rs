409cde86d8c5e3f4a2fa6b9799b08feeab108160
tyler569 <t5j6p9@gmail.com>
2015-05-22 07:44:27 -0400

    Changed refs to owned Strings and it works now + PING

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-#![feature(collections)]
 #![feature(io)]
 
 use std::net::{TcpStream};
 use std::io::{BufRead, BufReader, Write, stdout};
 
 mod parser;
+use parser::Line;
 
 fn main() {
 
 	let name = "irc.freenode.net:6667";	
-	let mut con = TcpStream::connect(name).ok().unwrap();
-	let mut rbuf = BufReader::new(con.try_clone().ok().unwrap());
+	let con = TcpStream::connect(name).ok().unwrap();
+	let rbuf = BufReader::new(con.try_clone().ok().unwrap());
 	let mut wbuf = con.broadcast(stdout());
 	write!(&mut wbuf, "NICK tbot_3\r\n").ok().unwrap();
 	write!(&mut wbuf, "USER tbot_3 0 * :tbot - new and improved\r\n").ok().unwrap();
 	write!(&mut wbuf, "JOIN #openredstone\r\n").ok().unwrap();
 	for line in rbuf.lines() {
-        let mut lp = parser::Line::new();
         let linestr = line.ok().unwrap();
-        lp.parse(linestr);
+        let lp = Line::parse(linestr).ok().unwrap();
         println!("{:?} {:?} {:?}", lp.prefix(), lp.command(), lp.params());
+        if lp.command() == "PING" {
+            write!(&mut wbuf, "PONG :{}\n", lp.params().last().unwrap()).ok().unwrap();
+        }
 	}
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,58 +1,56 @@
 
-pub struct Line<'a> {
+pub struct Line {
     raw:      String,
-    prefix:   Option<&'a str>,
-    command:  &'a str,
-    params:   Vec<&'a str>,
+    prefix:   Option<String>,
+    command:  String,
+    params:   Vec<String>,
 }
 
-impl<'a> Line<'a> {
-    pub fn new() -> Line<'a> {
-        Line {
-            raw: String::new(),
+impl Line {
+    pub fn parse(line: String) -> Result<Line, &'static str> {
+        let mut ret = Line { 
+            raw: line,
             prefix: None,
-            command: "",
-            params: Vec::new(),
+            command: String::new(),
+            params: Vec::new()
+        };
+        {
+            if ret.raw.len() == 0 {
+                return Err("Empty Line");
+            }
+
+            let mut splc: Vec<_>;
+            if &ret.raw[0..1] == ":" {
+                let splp: Vec<_> = ret.raw.splitn(2, ' ').collect();
+                ret.prefix = Some(splp[0].to_string());
+                splc = splp[1].splitn(2, " :").collect();
+            } else {
+                ret.prefix = None;
+                splc = ret.raw.splitn(2, " :").collect();
+            }
+            let cmd_params: Vec<_> = splc[0].split(' ').collect();
+            ret.command = cmd_params[0].to_string();
+            for param in &cmd_params[1..] {
+                ret.params.push(param.to_string());
+            }
+            if splc.len() == 2 {
+                ret.params.push(splc[1].to_string());
+            }
         }
+
+        Ok(ret)
     }
 
-    pub fn parse(&'a mut self, line: String) -> Result<(), &'static str> {
-
-        if line.len() == 0 {
-            return Err("Empty Line");
-        }
-
-        self.raw = line;
-        
-        let mut splc: Vec<_>;
-        if &self.raw[0..1] == ":" {
-            let splp: Vec<_> = self.raw.splitn(2, ' ').collect();
-            self.prefix = Some(splp[0]);
-            splc = splp[1].splitn(2, " :").collect();
-        } else {
-            self.prefix = None;
-            splc = self.raw.splitn(2, " :").collect();
-        }
-        let cmd_params: Vec<_> = splc[0].split(' ').collect();
-        self.command = cmd_params[0];
-        self.params.push_all(cmd_params.tail());
-        if splc.len() == 2 {
-            self.params.push(splc[1]);
-        }
-
-        Ok(())
-    }
-
-    pub fn raw(&'a self) -> &'a String {
+    pub fn raw(&self) -> &String {
         &self.raw
     }
-    pub fn prefix(&'a self) -> Option<&'a str> {
-        self.prefix
+    pub fn prefix(&self) -> &Option<String> {
+        &self.prefix
     }
-    pub fn command(&'a self) -> &'a str {
-        self.command
+    pub fn command(&self) -> &String {
+        &self.command
     }
-    pub fn params(&'a self) -> &'a Vec<&'a str> {
+    pub fn params(&self) -> &Vec<String> {
         &self.params
     }
 }
@@ -65,8 +63,7 @@ mod tests {
     #[should_panic(expected = "Empty Line")]
     fn parse_empty() {
         let test = "".to_string();
-        let mut line = Line::new();
-        match line.parse(test) {
+        match Line::parse(test) {
             Ok(_) => { panic!("It worked, fail") },
             Err(_) => { panic!("Empty Line") },
         }
@@ -75,9 +72,7 @@ mod tests {
     #[test]
     fn parse_ping() {
         let test = "PING :Hello World".to_string();
-        let mut line = Line::new();
-        line.parse(test);
-
+        let line = Line::parse(test).ok().unwrap();
         assert_eq!(line.raw, "PING :Hello World".to_string());
         assert_eq!(line.command, "PING");
         assert_eq!(line.params, vec!["Hello World"]);
